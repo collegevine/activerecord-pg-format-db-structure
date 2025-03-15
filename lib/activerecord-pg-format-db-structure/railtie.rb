f1803daaedd8848fb2d368bdb44107dd201c2847
Jell <jean-louis@reify.se>
2025-03-15 10:42:58 +0100

    Add rails config
    in case someone wants to override the default appender & customize spacing

--- lib/activerecord-pg-format-db-structure/railtie.rb
+++ lib/activerecord-pg-format-db-structure/railtie.rb
@@ -6,6 +6,7 @@ module ActiveRecordPgFormatDbStructure
     config.activerecord_pg_format_db_structure = ActiveSupport::OrderedOptions.new
     config.activerecord_pg_format_db_structure.transforms = DEFAULT_TRANSFORMS.dup
     config.activerecord_pg_format_db_structure.deparser = DEFAULT_DEPARSER
+    config.activerecord_pg_format_db_structure.statement_appender = DEFAULT_STATEMENT_APPENDER
 
     rake_tasks do
       load "activerecord-pg-format-db-structure/tasks/clean_db_structure.rake"
